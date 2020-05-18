ffb3c93b300b98fabfbf5d74c4135d786c3c43d6
Hunter Stern <hunter.stern@autodesk.com>
2020-05-17 17:16:07 -0700

    more changes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { getLabels, getFilteredMessages, getMessage } = require('./authedactions');
+
+module.exports = {
+    gmail: {
+        getLabels,
+        getFilteredMessages,
+        getMessage
+    }
+}
